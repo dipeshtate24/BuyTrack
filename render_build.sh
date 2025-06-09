d0ed9bec57c071ad76172cd2d7268e7ad3a9a23d
dipeshtate24 <74396369+dipeshtate24@users.noreply.github.com>
2025-06-09 18:46:57 +0530

    Create render_build.sh

---
+++ render_build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Install Chrome manually
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get update
+apt-get install -y ./google-chrome-stable_current_amd64.deb
+
+# Install Python dependencies
+pip install -r requirements.txt
