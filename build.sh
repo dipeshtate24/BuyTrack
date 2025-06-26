da4935b9fd2f045e5984933a1f72fa3f4b0bbcd0
dipeshtate24 <74396369+dipeshtate24@users.noreply.github.com>
2025-06-26 11:36:53 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
-set -x
+set -e
 
-# Update and install necessary tools
+# Install dependencies
 apt-get update
 apt-get install -y wget unzip curl gnupg
 
 # Install Google Chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt install -y ./google-chrome-stable_current_amd64.deb
+wget -q -O google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install -y ./google-chrome.deb
 
-# Find installed Chrome version
+# Get Chrome version
 CHROME_VERSION=$(google-chrome --version | grep -oP '\d+\.\d+\.\d+')
 
-# Download matching ChromeDriver (Chrome for Testing version)
-wget -O chromedriver.zip "https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/${CHROME_VERSION}/linux64/chromedriver-linux64.zip"
+# Download ChromeDriver that matches Chrome
+wget -q -O chromedriver.zip "https://storage.googleapis.com/chrome-for-testing-public/${CHROME_VERSION}/linux64/chromedriver-linux64.zip"
+
+# Unpack and move
 unzip chromedriver.zip
-mv chromedriver-linux64/chromedriver /usr/local/bin/
+mv chromedriver-linux64/chromedriver /usr/local/bin/chromedriver
 chmod +x /usr/local/bin/chromedriver
